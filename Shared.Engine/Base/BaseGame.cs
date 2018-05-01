b543ffd0fc1d218f9041d7f822559e53a1a14eb7
daniel <dany_the_man@hotmail.com>
2018-05-01 12:43:08 +0100

    Preparing Rendering and Base game initialzation

---
+++ Shared.Engine/Base/BaseGame.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Engine.Base
+{
+    public abstract class BaseGame
+    {
+        // Target solution for the game
+        public abstract Vector2 _v2IntialResolution { get;  }
+        // target the solution for the game
+        protected BaseGame()
+        {
+
+        }
+
+        // Initializes the game
+        public virtual void Init()
+        {
+            Renderer.Instance.Init(_v2IntialResolution);
+        }
+
+        // Draws our objects // this is internal to this class only in case there are issues Drawing objects
+        internal void Draw()
+        {
+            Renderer.Instance.Draw();
+        }
+
+    }
+}
---
+++ Shared.Engine/Base/Renderer.cs
@@ -0,0 +1,44 @@
+﻿using OpenTK;
+using OpenTK.Graphics.ES30;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Engine.Base
+{
+    public class Renderer
+    {
+        // target dimensions of the display
+        private Vector2 _v2TargetDimension;
+
+        //Instance of Renderer
+        private static Renderer _Instance;
+
+        //Singleton of Renderer
+        public static Renderer Instance => _Instance ?? (_Instance = new Renderer());
+
+        //Dimensions of Target Display
+        public Vector2 TargetDimensions => _v2TargetDimension;
+
+        //Constuctor
+        private Renderer()
+        {
+
+        }
+
+        //Create the renderer
+        // <params> name=targetDimensions
+        internal void Init(Vector2 targetDimensions)
+        {
+            _v2TargetDimension = targetDimensions;
+        }
+
+        //This will draw all objects of our game
+        internal void Draw()
+        {
+            GL.ClearColor(0f, 0.4f, 0f, 0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        }
+
+    }
+}
